3655655f64ecbdc53ad1dca9f70332215ea88026
harsh-coder-desgin <harshpatel51057@gmail.com>
2026-02-04 22:47:00 +0530

    Setup to all file in page index add

---
+++ frontend/src/pages/indexpage.js
@@ -0,0 +1,31 @@
+import AddCourse from './AddCourse'
+import AllCourse from './AllCourse'
+import AllCreator from './AllCreator'
+import CourseDetail from './CourseDetail'
+import CreatorLogin from './CreatorLogin'
+import CreatorProfile from './CreatorProfile'
+import CreatorSignup from './CreatorSignup'
+import Dashboard from './Dashboard'
+import EditCourse from './EditCourse'
+import ExploreCourse from './ExploreCourse'
+import Home from './Home'
+import ReadReview from './ReadReview'
+import SearchResult from './SearchResult'
+import Setting from './Setting'
+
+export {
+    AddCourse,
+    AllCourse,
+    AllCreator,
+    CourseDetail,
+    CreatorLogin,
+    CreatorProfile,
+    CreatorSignup,
+    Dashboard,
+    EditCourse,
+    ExploreCourse,
+    Home,
+    ReadReview,
+    SearchResult,
+    Setting
+}
