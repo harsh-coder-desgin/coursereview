9735eb0bbbf30e25046c9dc45f493da41dd02740
harsh-coder-desgin <harshpatel51057@gmail.com>
2026-02-04 22:25:36 +0530

    Adding file in index

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,6 +1,6 @@
 import Container from "./Container/Container"
 import Footer from "./Footer/Footer"
-import CreaterHeder from "./Header/CreaterHeader"
+import CreaterHeader from "./Header/CreaterHeader"
 import UserHeader from "./Header/UserHeader"
 import CreatorLogoutbtn from "./Header/CreatorLogoutbtn"
 import UserLogoutbtn from "./Header/UserLogoutbtn"
@@ -9,10 +9,31 @@ import Input from "./Input"
 import Button from "./Button"
 import CreatorAuthLayout from "./CreatorAuthLayout"
 import Textarea from "./Textarea"
+import CourseCard from './CourseCard'
+import CreatorHeading from './CreatorHeading'
+import Login from './Login'
+import RecentnewReview from "./RecentnewReview"
+import ReviewBox from './ReviewBox'
+import Select from './Select'
+import Signup from './Signup'
+import StarComponent from './StarComponent'
+import UserAuthLayout from './UserAuthLayout'
+import VerfiyAuth from './VerfiyAuth'
+import Verfiyemail from "./Verfiyemail"
+import ViewCourseDetail from "./ViewCourseDetail"
+import CourseRatingChart from "./Chart/CourseRatingChart"
+import OverReviewChart from "./Chart/OverReviewChart"
+import RatingChart from "./Chart/RatingChart"
+import AdvanceInformation from "./CreateCourseatab/AdvanceInformation"
+import BasicInformation from "./CreateCourseatab/BasicInformation"
+import PriceingCourse from "./CreateCourseatab/PriceingCourse"
+import PublishCourse from "./CreateCourseatab/PublishCourse"
+import CreateCourseHeader from "./Header/CreateCourseHeader"
+
 export{
     Container,
     Footer,
-    CreaterHeder,
+    CreaterHeader,
     UserHeader,
     CreatorLogoutbtn,
     UserLogoutbtn,
@@ -20,5 +41,25 @@ export{
     Input,
     Button,
     CreatorAuthLayout,
-    Textarea
+    Textarea,
+    CourseCard,
+    CreatorHeading,
+    Login,
+    RecentnewReview,
+    ReviewBox,
+    Select,
+    Signup,
+    StarComponent,
+    UserAuthLayout,
+    VerfiyAuth,
+    Verfiyemail,
+    ViewCourseDetail,
+    RatingChart,
+    OverReviewChart,
+    CourseRatingChart,
+    PublishCourse,
+    PriceingCourse,
+    BasicInformation,
+    AdvanceInformation,
+    CreateCourseHeader
 }
